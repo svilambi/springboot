a30b45738c9a26a445b8f7fc5545b0dc76cbd8b2
SVilambi <svilambi@gmail.com>
2023-05-26 08:06:23 -0500

    added terraform files

---
+++ terraform/vars.tf
@@ -0,0 +1,22 @@
+variable "instance_count" {
+  default = "1"
+}
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "aws_region" {
+  default = "us-east-2"
+}
+
+variable "aws_region_az" {
+  default = "us-east-2a"
+}
+
+variable "access_key" {
+  default = ${{ secrets.TERRAFORM_ACCESS_KEY }}
+}
+
+variable "secret_key" {
+  default = ${{ secrets.TERRAFORM_SECRET_KEY }}
+}
